9838a87be92a9c0f75c944a2ae872cebdb489a13
MPlemmons <mplemmons@mail.csuchico.edu>
2019-02-20 11:29:43 -0800

    fixed bug in code and added comments

--- main.cpp
+++ main.cpp
@@ -3,19 +3,21 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+//function initializations
 void sortDescending(int,int,int);
 void swap(int&,int&);
 
 int main()
 {
+  //initialize variables
   int numA, numB, numC;
+  //ask for user input
   cout<<"Enter any three numbers: ";
   cin>>numA>>numB>>numC;
 
+  //sort the 3 numbers in descending order
   sortDescending(numA, numB, numC);
   
-  cout<<"From greatest to least, they are: ";
-  cout<<numA<<","<<numB<<","<<numC<<endl;
   return 0;
 }
 
@@ -33,6 +35,10 @@ void sortDescending(int first, int second, int third)
   {
     swap(second,third);
   }
+
+  //print out descending
+  cout<<"From greatest to least, they are: ";
+  cout<<first<<","<<second<<","<<third<<endl;
 }
 
 void swap(int &first, int &second)
